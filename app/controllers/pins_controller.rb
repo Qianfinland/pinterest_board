35afee39e07854bd2301cd22015298bc0161d328
Qianfinland <qianfinland@gmail.com>
2015-02-17 00:22:46 +0200

    get image upload a kind of working

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -41,7 +41,7 @@ class PinsController < ApplicationController
   private
 
   def pin_params
-    params.require(:pin).permit(:title, :description)
+    params.require(:pin).permit(:title, :description, :image)
   end
 
   #need to call in show, destroy etc, so define here
